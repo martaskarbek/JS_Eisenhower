b79865e530034a108097335fdf585204f22a46e7
Marta Skarbek <marta@skarbek.name>
2020-07-21 11:00:19 +0200

    getting value

---
+++ main_logic.js
@@ -0,0 +1,24 @@
+
+const removeButton = document.querySelector('delete');
+const markAsDone = document.querySelector('.mark_as_done');
+const UItasks = document.querySelector('.ui');
+const UNtaskas = document.querySelector('.un');
+const NItasks = document.querySelector('.ni');
+const NNtaskas = document.querySelector('.nn');
+
+function main() {
+    getNewTaskContent();
+}
+
+function getNewTaskContent() {
+    const addButton = document.querySelectorAll('.add_task');
+    let inputContent = document.querySelector('.task_holder');
+
+    addButton.forEach(button => button.addEventListener('click', function() {
+        let taskValue = inputContent.value;
+        console.log(document.querySelector('.task_holder').value);
+    }))
+
+}
+
+main();

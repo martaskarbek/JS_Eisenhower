21b4c6364e472d83cc2eea844d3e5222cdeba161
mikolaj <mikolaj.urbanek@gmail.com>
2020-07-21 21:59:25 +0200

    local storage works in UI quarter

--- main_logic.js
+++ main_logic.js
@@ -23,14 +23,17 @@ function getNewTaskContent() {
             localStorage.setItem('ui', JSON.stringify(storageContentUI));
         }
         if (quarterName === "un") {
+            storageContentUI = JSON.parse(localStorage.getItem("un"));
             storageContentUN.push(inputValue.value);
             localStorage.setItem('un', JSON.stringify(storageContentUN));
         }
         if (quarterName === "ni") {
+            storageContentUI = JSON.parse(localStorage.getItem("ni"));
             storageContentNI.push(inputValue.value);
             localStorage.setItem('ni', JSON.stringify(storageContentNI));
         }
         if (quarterName === "nn") {
+            storageContentUI = JSON.parse(localStorage.getItem("nn"));
             storageContentNN.push(inputValue.value);
             localStorage.setItem('nn', JSON.stringify(storageContentNN));
         }
@@ -59,11 +62,24 @@ function getDataFromStorage() {
     let unData = JSON.parse(localStorage.getItem("un"));
     let nnData = JSON.parse(localStorage.getItem("nn"));
 
+
         for (let j=0; j<uiData.length; j++) {
             let inputValue2 = uiData[j];
             console.log(document.getElementsByClassName("quarter").item(0));
             createNewTask(inputValue2, document.querySelector('#ui'));
-        };
+        }
+    for (let k = 0; k < unData.length; k++) {
+        let inputValue3 = unData[k];
+        createNewTask(inputValue3, document.querySelector('#un'));
+    }
+    for (let n = 0; n < niData.length; n++) {
+        let inputValue4 = niData[n];
+        createNewTask(inputValue4, document.querySelector('#ni'));
+    }
+    for (let m = 0; m < nnData.length; m++) {
+        let inputValue5 = nnData[m];
+        createNewTask(inputValue5, document.querySelector('#nn'));
+    }
 
 
 }

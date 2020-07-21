60268c05f92a8cd447f430eedf95f665ba569722
mikolaj <mikolaj.urbanek@gmail.com>
2020-07-22 00:01:56 +0200

    fixed vanishing problem with localStorage

--- main_logic.js
+++ main_logic.js
@@ -1,5 +1,6 @@
 const removeButton = document.querySelector('delete');
 const markAsDone = document.querySelector('.mark_as_done');
+
 let storageContentUI = [];
 let storageContentUN = [];
 let storageContentNI = [];
@@ -7,41 +8,46 @@ let storageContentNN = [];
 const storageKeys = ['ui', 'un', 'nn', 'ni'];
 
 function main() {
-    getNewTaskContent();
+    addNewTask();
     getDataFromStorage();
+
 }
 
-function getNewTaskContent() {
+function addNewTask() {
     const addButton = document.querySelectorAll('.add_task');
     addButton.forEach(button => button.addEventListener('click', function() {
         const inputValue = this.parentElement.getElementsByClassName('task_holder').item(0);
-        createNewTask(inputValue.value, inputValue.parentElement.parentElement);
         const quarterName = this.parentElement.parentElement.id;
-        if (quarterName === "ui") {
-            storageContentUI = JSON.parse(localStorage.getItem("ui"));
+        console.log(quarterName);
+        createNewTask(inputValue.value, inputValue.parentElement.parentElement);
+        if(quarterName === 'ui'){
+            if(localStorage.getItem("ui") !== null){
+                storageContentUI = JSON.parse(localStorage.getItem("ui"));}
             storageContentUI.push(inputValue.value);
-            localStorage.setItem('ui', JSON.stringify(storageContentUI));
-        }
+            localStorage.setItem('ui', JSON.stringify(storageContentUI));}
         if (quarterName === "un") {
-            storageContentUI = JSON.parse(localStorage.getItem("un"));
+            if(localStorage.getItem("un") !== null){
+                storageContentUN = JSON.parse(localStorage.getItem("un"));}
             storageContentUN.push(inputValue.value);
             localStorage.setItem('un', JSON.stringify(storageContentUN));
         }
         if (quarterName === "ni") {
-            storageContentUI = JSON.parse(localStorage.getItem("ni"));
+            if(localStorage.getItem("ni") !== null){
+                storageContentNI = JSON.parse(localStorage.getItem("ni"));}
             storageContentNI.push(inputValue.value);
             localStorage.setItem('ni', JSON.stringify(storageContentNI));
         }
         if (quarterName === "nn") {
-            storageContentUI = JSON.parse(localStorage.getItem("nn"));
+            if(localStorage.getItem("nn") !== null){
+                storageContentNN = JSON.parse(localStorage.getItem("nn"));}
             storageContentNN.push(inputValue.value);
             localStorage.setItem('nn', JSON.stringify(storageContentNN));
         }
     }))
 }
 
+
 function createNewTask(inputValue, targetNode) {
-    console.log(inputValue);
     const createTask = function(inputValue){
         const template = document.querySelector('#task-template');
         const clone = document.importNode(template.content, true);
@@ -53,7 +59,9 @@ function createNewTask(inputValue, targetNode) {
     };
     const task = createTask(inputValue);
     targetNode.appendChild(task);
+
 }
+
 
 function getDataFromStorage() {
     const notesShow =  document.querySelectorAll('.quarter');
@@ -62,23 +70,29 @@ function getDataFromStorage() {
     let unData = JSON.parse(localStorage.getItem("un"));
     let nnData = JSON.parse(localStorage.getItem("nn"));
 
-
-        for (let j=0; j<uiData.length; j++) {
+    if(uiData !== null) {
+        for (let j = 0; j < uiData.length; j++) {
             let inputValue2 = uiData[j];
-            console.log(document.getElementsByClassName("quarter").item(0));
             createNewTask(inputValue2, document.querySelector('#ui'));
         }
-    for (let k = 0; k < unData.length; k++) {
-        let inputValue3 = unData[k];
-        createNewTask(inputValue3, document.querySelector('#un'));
     }
-    for (let n = 0; n < niData.length; n++) {
-        let inputValue4 = niData[n];
-        createNewTask(inputValue4, document.querySelector('#ni'));
+    if(unData !== null) {
+        for (let k = 0; k < unData.length; k++) {
+            let inputValue3 = unData[k];
+            createNewTask(inputValue3, document.querySelector('#un'));
+        }
     }
-    for (let m = 0; m < nnData.length; m++) {
-        let inputValue5 = nnData[m];
-        createNewTask(inputValue5, document.querySelector('#nn'));
+    if(niData !== null) {
+        for (let n = 0; n < niData.length; n++) {
+            let inputValue4 = niData[n];
+            createNewTask(inputValue4, document.querySelector('#ni'));
+        }
+    }
+    if(nnData !== null) {
+        for (let m = 0; m < nnData.length; m++) {
+            let inputValue5 = nnData[m];
+            createNewTask(inputValue5, document.querySelector('#nn'));
+        }
     }
 
 

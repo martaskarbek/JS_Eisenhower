1e05810d0dc02d39e2ec3d071375e19d353ffa5b
Marta Skarbek <marta@skarbek.name>
2020-07-22 09:20:13 +0200

    changed style for byttons

--- main_logic.js
+++ main_logic.js
@@ -1,3 +1,4 @@
+
 const removeButton = document.querySelector('delete');
 const markAsDone = document.querySelector('.mark_as_done');
 let storageContentUI = [];
@@ -18,27 +19,32 @@ function getNewTaskContent() {
         createNewTask(inputValue.value, inputValue.parentElement.parentElement);
         const quarterName = this.parentElement.parentElement.id;
         if (quarterName === "ui") {
-            storageContentUI = JSON.parse(localStorage.getItem("ui"));
-            storageContentUI.push(inputValue.value);
+            storageContentUI2 = JSON.parse(localStorage.getItem("ui"));
+            console.log(inputValue);
+            console.log(String(inputValue.value));
+            for (let aa = 0; aa <storageContentUI2.length; aa++){
+                let a = storageContentUI2[aa];
+                storageContentUI.push('a');}
+        /*    storageContentUI.push(inputValue.value);*/
             localStorage.setItem('ui', JSON.stringify(storageContentUI));
+            console.log(storageContentUI);
         }
         if (quarterName === "un") {
-            storageContentUN.push(inputValue.value);
+            /*storageContentUN.push(inputValue.value);*/
             localStorage.setItem('un', JSON.stringify(storageContentUN));
         }
         if (quarterName === "ni") {
-            storageContentNI.push(inputValue.value);
+           /* storageContentNI.push(inputValue.value);*/
             localStorage.setItem('ni', JSON.stringify(storageContentNI));
         }
         if (quarterName === "nn") {
-            storageContentNN.push(inputValue.value);
+ /*           storageContentNN.push(inputValue.value);*/
             localStorage.setItem('nn', JSON.stringify(storageContentNN));
         }
     }))
 }
 
 function createNewTask(inputValue, targetNode) {
-    console.log(inputValue);
     const createTask = function(inputValue){
         const template = document.querySelector('#task-template');
         const clone = document.importNode(template.content, true);
@@ -53,17 +59,17 @@ function createNewTask(inputValue, targetNode) {
 }
 
 function getDataFromStorage() {
-    const notesShow =  document.querySelectorAll('.quarter');
+/*    const notesShow =  document.querySelectorAll('.quarter');
     let uiData = JSON.parse(localStorage.getItem("ui"));
     let niData = JSON.parse(localStorage.getItem("ni"));
     let unData = JSON.parse(localStorage.getItem("un"));
     let nnData = JSON.parse(localStorage.getItem("nn"));
 
-        for (let j=0; j<uiData.length; j++) {
-            let inputValue2 = uiData[j];
-            console.log(document.getElementsByClassName("quarter").item(0));
-            createNewTask(inputValue2, document.querySelector('#ui'));
-        };
+    for (let j=0; j<uiData.length; j++) {
+        let inputValue2 = uiData[j];
+        document.getElementsByClassName("quarter").item(0);
+        createNewTask(inputValue2, document.querySelector('#ui'));*/
+/*    };*/
 
 
 }
@@ -73,3 +79,19 @@ function removeTask() {
 }
 
 main();
+/*
+function getDataFromStorage() {
+    let restoredData = JSON.parse(localStorage.getItem("ui"));
+    for (let i=0; i<storageKeys.length; i++) {
+        let targetNode = storageKeys[i];
+        if (restoredData.length != 0){
+            for (let j = 0; j < restoredData.length; j++) {
+                /!*inputValue = notesShow[i];*!/
+                let inputValue2 = uiData[j];
+                console.log(document.getElementsByClassName("quarter").item(0));
+                console.log(createNewTask(inputValue2, document.querySelector(`#${targetNode}`)));
+            };
+        };
+    };
+}*/
+
